4052d3b1e286b9a9d155096935b56e60c9882408
Kenny Lee <kenny.lee927@gmail.com>
2022-08-11 03:27:50 -0400

    setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,41 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    mode: "production",
-    entry: "./src/index.js",
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Odin To-do List',
+      }),
+    ],  
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
         ],
-      },
+    },
 };
