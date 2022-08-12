3344fcb3773c6a9ddfdaeac3cdc5410e8e629ae3
Kenny Lee <kenny.lee927@gmail.com>
2022-08-12 04:20:04 -0400

    write preliminary displayUI module to load the page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import './styles/style.css';
-import notifBell from "./images/notif_Bell.svg"
+import {displayHeader, displaySidebar} from "./modules/displayUI";
+displayHeader("Ken");
+displaySidebar(["Project", "Another", "And another"]);
 
 console.log("hello");
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
   
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = notifBell;
+//    // Add the image to our existing div.
+//    const myIcon = new Image();
+//    myIcon.src = notifBell;
  
-   element.appendChild(myIcon);
+//    element.appendChild(myIcon);
  
-    return element;
-  }
+//     return element;
+//   }
  
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
---
+++ src/modules/displayUI.js
@@ -0,0 +1,211 @@
+import notif_Bell from "../images/notif_Bell.svg";
+
+export function displayHeader(name, icon) {
+    // header container
+    const header = document.createElement("header");
+
+    // >app logo and title (left)
+    const logo = document.createElement("h1");
+    logo.textContent = "To-Do";
+
+    // >right side container
+    const notifications = document.createElement("div");
+    notifications.id = "notifications";
+
+    const notifBell = document.createElement("img");
+    notifBell.id = "notifBell";
+    notifBell.src = notif_Bell;
+
+    // >> user profile section 
+    const userProfile = document.createElement("div");
+    userProfile.id = "userProfile";
+    // >>> welcome msg
+    const userMsg = document.createElement("div");
+    userMsg.textContent = "Welcome, " + name;
+    // >>> icon image
+    const userIcon = document.createElement("img");
+    userIcon.id = "userIcon";
+    userIcon.src = "https://avatars.githubusercontent.com/u/89105909?v=4";
+
+    userProfile.append(userMsg, userIcon);
+    notifications.append(notifBell, userProfile);
+
+    // DOM functions to append to page
+    header.append(logo, notifications);
+    document.body.appendChild(header);
+}
+
+export function displaySidebar (projects) {
+    // sidebar container
+    const sidebar = document.createElement("div");
+    sidebar.id="sidebar";
+
+    // >sidebar links
+    const sidebarLinks = document.createElement("ul");
+    sidebarLinks.id="sidebar-links";
+
+    const today = document.createElement("li");
+    today.textContent = "Today";
+
+    const projectList = document.createElement("li");
+    projectList.textContent="Projects";
+
+    const projectListList = document.createElement("ul");
+
+    projects.forEach (project => {
+        let thisProject = document.createElement("li");
+        thisProject.textContent = project;
+        projectListList.appendChild(thisProject);
+    })
+
+    projectList.appendChild(projectListList);
+    sidebarLinks.append(today, projectList);
+
+
+    const sidebarBottom = document.createElement("ul");
+    sidebarBottom.id="sidebar-bottom";
+    const settings = document.createElement("li");
+    settings.textContent="Settings";
+    const logOut = document.createElement("li");
+    logOut.textContent="Log Out";
+    sidebarBottom.append(settings, logOut);
+
+
+    sidebar.append(sidebarLinks, sidebarBottom);
+    document.body.appendChild(sidebar);
+}
+
+
+
+/* <body>
+    <div id="sidebar">
+        <ul id="sidebar-links">
+            <li>Today</li>
+            <li>
+                Projects
+                <ul>
+                    <li>Sample project</li>
+                    <li>Sample project 2</li>
+                    <li>Sample project 3</li>
+                </ul>
+            </li>
+            <li>Completed</li>
+        </ul>
+
+        <ul id="sidebar-bottom">
+            <li>Settings</li>
+            <li>Log out</li>
+        </ul>
+    </div>
+
+    <div id="aside">
+        <h2>This Week</h2>
+        <ul id="calendar">
+            <li class="calendarDay">Monday</li>
+            <li class="calendarDay">Tuesday</li>
+            <li class="calendarDay">Wednesday</li>
+            <li class="calendarDay">Thursday</li>
+            <li class="calendarDay">Friday</li>
+            <li class="calendarDay">Saturday</li>
+            <li class="calendarDay">Sunday</li>
+        </ul>
+    </div>
+
+    <div id="main-content">
+        <div id="main-header">
+            <h2>All Projects</h2>
+            <button>New Task</button>
+        </div>
+        <div id="cards">
+            <div class="card">
+                <h3 class="cardHeader">
+                    <span>
+                        <input type="checkbox" checked="checked" />
+                        Test card header
+                    </span>
+                    <div>
+                        <button>Archive</button>
+                        <button>Delete</button>
+                    </div>
+                </h3>
+                <p>Due: August 20, 2022</p>
+                <p>
+                    Test card description goes here. Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolore suscipit laudantium
+                    nulla vero dolores magnam saepe, neque perferendis fugit
+                    doloribus quod vel necessitatibus impedit soluta voluptates
+                    reprehenderit sint molestias. Quasi!
+                </p>
+            </div>
+            <div class="card">
+                <h3 class="cardHeader">
+                    <span>
+                        <input type="checkbox" checked="checked" />
+                        Test card header
+                    </span>
+                    <div>
+                        <button>Archive</button>
+                        <button>Delete</button>
+                    </div>
+                </h3>
+                <p>Due: August 20, 2022</p>
+                <p>
+                    Test card description goes here. Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolore suscipit laudantium
+                    nulla vero dolores magnam saepe, neque perferendis fugit
+                    doloribus quod vel necessitatibus impedit soluta voluptates
+                    reprehenderit sint molestias. Quasi!
+                </p>
+            </div>
+            <div class="card">
+                <h3 class="cardHeader">
+                    <span>
+                        <input type="checkbox" checked="checked" />
+                        Test card header
+                    </span>
+                    <div>
+                        <button>Archive</button>
+                        <button>Delete</button>
+                    </div>
+                </h3>
+                <p>Due: August 20, 2022</p>
+                <p>
+                    Test card description goes here. Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolore suscipit laudantium
+                    nulla vero dolores magnam saepe, neque perferendis fugit
+                    doloribus quod vel necessitatibus impedit soluta voluptates
+                    reprehenderit sint molestias. Quasi!
+                </p>
+            </div>
+            <div class="card">
+                <h3 class="cardHeader">
+                    <span>
+                        <input type="checkbox" checked="checked" />
+                        Test card header
+                    </span>
+                    <div>
+                        <button>Archive</button>
+                        <button>Delete</button>
+                    </div>
+                </h3>
+                <p>Due: August 20, 2022</p>
+                <p>
+                    Test card description goes here. Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolore suscipit laudantium
+                    nulla vero dolores magnam saepe, neque perferendis fugit
+                    doloribus quod vel necessitatibus impedit soluta voluptates
+                    reprehenderit sint molestias. Quasi!
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>
+            Made by
+            <a href="https://github.com/kenkneelee">Kenny Lee</a>
+            for
+            <a href="https://www.theodinproject.com/">The Odin Project.</a>
+        </p>
+    </footer>
+</body> */
